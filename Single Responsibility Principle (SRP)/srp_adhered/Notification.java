7f083076398b0bff244f81b0fbe8ab077c6ed926
Sankalp Bhagwat <sankalp.29bhagwat@gmail.com>
2025-08-01 15:21:24 +0530

    Removed duplicate folder

--- Single Responsibility Principle (SRP)/srp_adhered/Notification.java
+++
@@ -1,7 +0,0 @@
-package srp_adhered;
-
-public class Notification {
-    public void sendNotification(String recepient, String message) {
-        System.out.println("Sending notification to " + recepient + " " + message);
-    }
-}
